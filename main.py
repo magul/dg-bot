de585a37c1a277a5a549415348bd6ccf97544818
Tomasz Magulski <tomasz@magul.ski>
2021-06-13 11:40:27 +0200

    coping images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import io
 import os
+import shutil
 import zipfile
 from time import sleep
 
@@ -11,10 +12,11 @@ import secrets
 
 language_transition = {
     # "cs-CZ": "cs",
-    "de-DE": "de",
+    # "de-DE": "de",
     # "es-ES": "es",
     # "fr-FR": "fr",
     # "hu-HU": "hu",
+    "hy-AM": "hy",
     # "it-IT": "it",
     # "ko-KR": "ko",
     # "pl-PL": "pl",
@@ -71,11 +73,11 @@ if __name__ == "__main__":
     repo = Repo(os.path.join(os.path.dirname(__file__), "tutorial"))
     c.build()
     for code in language_transition:
-        repo.heads.master.checkout()
-        repo.create_head("crowdin-translation-{}".format(language_transition[code]))
-        repo.heads[
-            "crowdin-translation-{}".format(language_transition[code])
-        ].checkout()
+        # repo.heads.master.checkout()
+        # repo.create_head("crowdin-translation-{}".format(language_transition[code]))
+        # repo.heads[
+        #     "crowdin-translation-{}".format(language_transition[code])
+        # ].checkout()
 
         with c.download(code) as zip_f:
             for src_name in zip_f.namelist():
@@ -104,3 +106,61 @@ if __name__ == "__main__":
                         )
                     ]
                 )
+
+            # refresh images
+            for image in [
+                ("css", "images", "bootstrap1.png"),
+                ("css", "images", "color2.png"),
+                ("css", "images", "final.png"),
+                ("css", "images", "font.png"),
+                ("css", "images", "margin2.png"),
+                ("deploy", "images", "github_get_repo_url_screenshot.png"),
+                ("deploy", "images", "new_github_repo.png"),
+                ("deploy", "images", "pythonanywhere_account.png"),
+                ("deploy", "images", "pythonanywhere_bash_console.png"),
+                ("deploy", "images", "pythonanywhere_beginner_account_button.png"),
+                ("deploy", "images", "pythonanywhere_create_api_token.png"),
+                ("django_admin", "images", "django_admin3.png"),
+                ("django_admin", "images", "edit_post3.png"),
+                ("django_admin", "images", "login_page2.png"),
+                ("django_forms", "images", "csrf2.png"),
+                ("django_forms", "images", "drafts.png"),
+                ("django_forms", "images", "edit_button2.png"),
+                ("django_forms", "images", "edit_form2.png"),
+                ("django_forms", "images", "form_validation2.png"),
+                ("django_forms", "images", "new_form2.png"),
+                ("django_forms", "images", "post_create_error.png"),
+                ("django_start_project", "images", "install_worked.png"),
+                ("django_templates", "images", "donut.png"),
+                ("django_templates", "images", "step1.png"),
+                ("django_templates", "images", "step2.png"),
+                ("django_templates", "images", "step3.png"),
+                ("django_urls", "images", "error1.png"),
+                ("django_urls", "images", "url.png"),
+                ("django_views", "images", "error.png"),
+                ("extend_your_application", "images", "404_2.png"),
+                ("extend_your_application", "images", "attribute_error2.png"),
+                ("extend_your_application", "images", "does_not_exist2.png"),
+                ("extend_your_application", "images", "no_reverse_match2.png"),
+                ("extend_your_application", "images", "post_detail2.png"),
+                ("extend_your_application", "images", "post_list2.png"),
+                ("extend_your_application", "images", "template_does_not_exist2.png"),
+                ("how_the_internet_works", "images", "internet_1.png"),
+                ("how_the_internet_works", "images", "internet_2.png"),
+                ("how_the_internet_works", "images", "internet_3.png"),
+                ("how_the_internet_works", "images", "internet_4.png"),
+                ("html", "images", "step1.png"),
+                ("html", "images", "step3.png"),
+                ("html", "images", "step4.png"),
+                ("html", "images", "step6.png"),
+                ("images", "application.png"),
+                ("python_installation", "images", "python-installation-options.png"),
+                ("python_installation", "images", "windows-plus-r.png"),
+                ("python_introduction", "images", "cupcake.png"),
+            ]:
+                src_path = os.path.join("tutorial", "en", *image)
+                dst_path = os.path.join("tutorial", language_transition[code], *image)
+                os.makedirs(os.path.dirname(dst_path), exist_ok=True)
+                shutil.copyfile(src_path, dst_path)
+
+                repo.index.add([os.path.join(language_transition[code], *image)])
